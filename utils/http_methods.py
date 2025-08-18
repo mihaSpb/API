4066a4638788132cb915efccd667259f70120a34
Mikhail <mihaspb2007@gmail.com>
2025-08-18 22:37:18 +0300

    Add class for customs methods and Python packages

---
+++ utils/http_methods.py
@@ -0,0 +1,26 @@
+import requests
+
+
+class HTTP_methods:
+    headers = {'Content-Type': 'application/json'}
+    cookie = ""
+
+    @staticmethod
+    def get(url):
+        result = requests.get(url, headers=HTTP_methods.headers, cookies=HTTP_methods.cookie)
+        return result
+
+    @staticmethod
+    def post(url, body):
+        result = requests.post(url, json = body, headers=HTTP_methods.headers, cookies=HTTP_methods.cookie)
+        return result
+
+    @staticmethod
+    def delete(url, body):
+        result = requests.delete(url, json = body, headers=HTTP_methods.headers, cookies=HTTP_methods.cookie)
+        return result
+
+    @staticmethod
+    def put(url, body):
+        result = requests.put(url, json = body, headers=HTTP_methods.headers, cookies=HTTP_methods.cookie)
+        return result
