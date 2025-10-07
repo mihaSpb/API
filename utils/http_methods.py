ef492acd6e53efa975b35a1bcf2356e784ee2270
Mikhail <mihaspb2007@gmail.com>
2025-10-07 10:11:49 +0300

    Update method PUT

--- utils/http_methods.py
+++ utils/http_methods.py
@@ -7,20 +7,39 @@ class HTTP_methods:
 
     @staticmethod
     def get(url):
-        result = requests.get(url, headers=HTTP_methods.headers, cookies=HTTP_methods.cookie)
+        result = requests.get(
+            url,
+            headers=HTTP_methods.headers,
+            cookies=HTTP_methods.cookie
+        )
         return result
 
     @staticmethod
     def post(url, body):
-        result = requests.post(url, json = body, headers=HTTP_methods.headers, cookies=HTTP_methods.cookie)
+        result = requests.post(
+            url,
+            json = body,
+            headers=HTTP_methods.headers,
+            cookies=HTTP_methods.cookie
+        )
         return result
 
     @staticmethod
     def delete(url, body):
-        result = requests.delete(url, json = body, headers=HTTP_methods.headers, cookies=HTTP_methods.cookie)
+        result = requests.delete(
+            url,
+            json = body,
+            headers=HTTP_methods.headers,
+            cookies=HTTP_methods.cookie
+        )
         return result
 
     @staticmethod
     def put(url, body):
-        result = requests.put(url, json = body, headers=HTTP_methods.headers, cookies=HTTP_methods.cookie)
+        result = requests.put(
+            url,
+            json = body,
+            headers=HTTP_methods.headers,
+            cookies=HTTP_methods.cookie
+        )
         return result
